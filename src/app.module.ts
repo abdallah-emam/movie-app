39a6e13636fed7f6139305da552b69695ddbc03d
Abdallah Hamdy <abdo.aa797@gmail.com>
2024-08-10 15:34:33 +0300

    Add config to run in different environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppLoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        dbName: 'movieAPIs',
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
